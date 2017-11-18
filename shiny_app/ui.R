b5d48c760fb40eef5df0d4bb10becb27b7d08e8e
nettog <gareth.netto@gmail.com>
2017-11-18 16:01:08 +0100

    feat: doggo inputs

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -21,8 +21,26 @@ shinyUI(navbarPage(
                        value = ""),
              selectizeInput(inputId = "dog_size", label = "Size of dog",
                             choices = c("", "Small", "Medium", "Large")),
+             numericInput(inputId = "dog_age", label = "Dogs age in years",
+                          value = 2),
+             textInput(inputId = "dog_desc", label = "Description of dog"),
+             textInput(inputId = "dog_tips", 
+                       label = "Tips for walking this dog"),
+             dateInput(inputId = "recieved_date", 
+                       label = "Date dog recieved to Shelter",
+                       format = "yyyy-mm-dd", startview = "year"),
+             selectizeInput(inputId = "status", 
+                         label = "Dog's availability to be walked",
+                         choices = c("", "Available", "Vets", "Training",
+                                     "Adopted")),
+             dateInput(inputId = "unavail_from", label = "Unavailable from", 
+                       format = "yyyy-mm-dd", startview = "year"),
+             dateInput(inputId = "unavail_to", label = "Unavailable to",
+                       format = "yyyy-mm-dd", startview = "year"),
              actionButton(inputId = "submit", label = "Submit", 
-                          class = "btn-primary"))),
+                          class = "btn-primary")
+             )
+           ),
   
   # toggle walker db ----
   tabPanel("Walker Database")
