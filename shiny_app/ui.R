f4b5da5be90e955847d8cd7f9e83bded8f55073e
nettog <gareth.netto@gmail.com>
2017-11-18 14:55:01 +0100

    feat: input div

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -9,7 +9,21 @@ library(shiny)
 
 shinyUI(navbarPage(
   title = "Animals In Need Foundation (Gibraltar)",
+  # book dog walk ----
   tabPanel("Book Dog Walk!"),
-  tabPanel("Doggo Database"),
+  
+  # toggle doggo db ----
+  tabPanel("Doggo Database",
+           div(
+             class = "jumbotron",
+             id = "doggo_form",
+             textInput(inputId = "dog_name", label = "Dog's name", 
+                       value = ""),
+             selectizeInput(inputId = "dog_size", label = "Size of dog",
+                            choices = c("", "Small", "Medium", "Large")),
+             actionButton(inputId = "submit", label = "Submit", 
+                          class = "btn-primary"))),
+  
+  # toggle walker db ----
   tabPanel("Walker Database")
 ))
