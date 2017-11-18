efc1165a7066d464ed48c875f721ad3e7dc7d612
nettog <gareth.netto@gmail.com>
2017-11-18 16:09:57 +0100

    feat: initialise walker form

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -43,5 +43,20 @@ shinyUI(navbarPage(
            ),
   
   # toggle walker db ----
-  tabPanel("Walker Database")
+  tabPanel("Walker Database",
+           div(
+             class = "jumbotron",
+             id = "walker_form",
+             fluidRow(column(width = 4,
+                             textInput(inputId = "walker_fname", 
+                                       label = "Forename", 
+                                       value = "")),
+                      column(width = 4,
+                             textInput(inputId = "walker_sname", 
+                                       label = "Surname", 
+                                       value = ""))),
+             actionButton(inputId = "submit", label = "Submit", 
+                          class = "btn-primary")
+           )
+  )
 ))
