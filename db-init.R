97f8c41b21fe31c09b32d49f52b5c8f40d5dfba5
nettog <gareth.netto@gmail.com>
2017-11-18 14:27:20 +0100

    feat: create dog_tbl

--- db-init.R
+++ db-init.R
@@ -4,7 +4,24 @@ rm(list=ls())
 library(dplyr)
 library(RSQLite)
 
-# Make SQLITE connection
+# Make SQLITE connection and doggodb ----
 sqlite <- dbDriver("SQLite")
 
-testdb <- dbConnect(sqlite, "shiny_app/testdb.db")
+doggodb <- dbConnect(sqlite, "shiny_app/doggo.db")
+
+# CREATE TABLE QUERIES ----
+dog_tbl <- data.frame(dog_id = integer(),
+                 name = character(),
+                 size = character(),
+                 age = integer(),
+                 description = character(),
+                 tips = character(),
+                 cover = character(),
+                 date_recieved = as.Date(character()),
+                 status = character())
+
+dbWriteTable(conn = doggodb,
+             name = "dog_tbl",
+             value = dog_tbl)
+
+walker_tbl <- data.frame()
